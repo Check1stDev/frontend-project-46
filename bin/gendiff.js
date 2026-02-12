62fa4c142c282b5671c55e4238cda80fe7da3633
Check1stDev <rez0n4ns@gmail.com>
2026-02-12 20:18:51 +0500

    Firs step

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+    .name('gendiff')
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
