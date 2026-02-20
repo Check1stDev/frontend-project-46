e2e9d8a4c74e4aca9e0b9c859f3ddc5ee580356c
Check1stDev <rez0n4ns@gmail.com>
2026-02-20 14:50:42 +0500

    implement stylish formatter and genDiff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -71,3 +71,111 @@ test ('test parser yml', () => {
 
 expect(result).toEqual(expected)
 });
+
+test ('compares flat new json files', () => {
+    const file1 = getFixturesPath('file1new.json');
+    const file2 = getFixturesPath('file2new.json');
+
+    const result = genDiff(file1,file2);
+
+    const expected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+expect(result).toBe(expected)
+})
+
+test ('compares flat new yaml files', () => {
+    const file1 = getFixturesPath('file1new.yml');
+    const file2 = getFixturesPath('file2new.yml');
+
+    const result = genDiff(file1,file2);
+
+    const expected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+expect(result).toBe(expected)
+})
--- src/index.js
+++ src/index.js
@@ -1,49 +1,76 @@
 const parseFilepath = require ('./parser.js');
 const _ = require('lodash');
 
-function formatStylish(fileDiff) {
+function formatStylish(fileDiff, depth = 1) {
+    const indent = ' '.repeat(((depth * 4)-2))
+    const indentNested = ' '.repeat(depth * 4)
+    const bracketIndent =  ' '.repeat((depth - 1) * 4)
    const result = fileDiff.reduce((acc,item) =>{
     if (item.type === 'removed'){
-        acc.push(`  - ${item.key}: ${item.value}`)
+        acc.push(`${indent}- ${item.key}: ${stringify(item.value,depth + 1)}`)
     } 
     else if (item.type === 'added') {
-        acc.push(`  + ${item.key}: ${item.value}`)
+        acc.push(`${indent}+ ${item.key}: ${stringify(item.value,depth + 1)}`)
+    }
+    else if (item.type === 'nested'){
+        acc.push(`${indentNested}${item.key}: ${formatStylish(item.children, depth + 1)}`)
     }
     else if (item.type === 'unchanged') {
-        acc.push(`    ${item.key}: ${item.value}`)
+        acc.push(`${indent}  ${item.key}: ${stringify(item.value,depth + 1)}`)
     }
     else if (item.type === 'changed') {
-        acc.push(`  - ${item.key}: ${item.before}`)
-        acc.push(`  + ${item.key}: ${item.after}`)
+        acc.push(`${indent}- ${item.key}: ${stringify(item.before,depth + 1)}`)
+        acc.push(`${indent}+ ${item.key}: ${stringify(item.after,depth + 1)}`)
     }
     return acc
    },[])
-   return `{\n${result.join('\n')}\n}`
+   return `{\n${result.join('\n')}\n${bracketIndent}}`
+}
+
+function stringify(value, depth = 1) {
+    if (!_.isPlainObject(value)) {
+        return String(value)
+    }
+    const indent = ' '.repeat(depth * 4)
+    const bracketIndent =  ' '.repeat((depth - 1) * 4)
+    
+    const lines = Object.entries(value)
+        .map(([key, val]) => `${indent}${key}: ${stringify(val, depth + 1)}` )
+
+    return `{\n${lines.join('\n')}\n${bracketIndent}}`
 }
 
 function genDiff(filepath1, filepath2) {
     const data1 = parseFilepath(filepath1);
     const data2 = parseFilepath(filepath2);
+    const tree = buildDiffThree(data1,data2)
+    return formatStylish(tree)
+}
+
+function buildDiffThree(data1,data2) {
     const allKeys = _.sortBy(_.union(_.keys(data1),_.keys(data2)));
     const result = [];
     for (const key of allKeys) {
         const key1 = _.has(data1, key);
         const key2 = _.has(data2, key);
+        const v1 = data1[key];
+        const v2 = data2[key];
         if (key1 && !key2) {
-            result.push({key, type:'removed', value: data1[key]});
+            result.push({key, type: 'removed', value: v1});
         } else if (!key1 && key2) {
-            result.push({key, type:'added', value: data2[key]});
-        }else if(key1 && key2) {
-            if (data1[key] === data2[key]) {
-                result.push({key, type:'unchanged', value: data2[key]});
+            result.push({key, type: 'added', value: v2});
+        } else if (_.isPlainObject(v1) && _.isPlainObject(v2)) {
+            result.push({key, type: 'nested', children: buildDiffThree(v1,v2)});
+        } 
+        else if(key1 && key2) {
+            if (v1 === v2) {
+                result.push({key, type: 'unchanged', value: v2});
             } else {
-                result.push({ key, type: 'changed', before: data1[key], after: data2[key] })
+                result.push({ key, type: 'changed', before: v1, after: v2 })
             }
         }
     }
-    return formatStylish(result)
+    return result
 }
-
-
 
 module.exports = genDiff;
